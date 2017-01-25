21be01e85bc24afcd8ef2496d1bbc7188133f2c1
David Eder <david.eder26@gmail.com>
2017-01-25 10:19:36 -0600

    Took out django.contrib.auth

--- bostonsalsa/settings.py
+++ bostonsalsa/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -122,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+)
