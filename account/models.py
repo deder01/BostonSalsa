ff476eff2f41c7ebe01925e1ba1d49ddf1ce7374
David Eder <david.eder26@gmail.com>
2017-01-25 11:03:11 -0600

    created account model

--- account/models.py
+++ account/models.py
@@ -1,26 +1,52 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.core.validators import RegexValidator
-from django.contrib.auth.base_user.models import AbstractBaseUser
+from django.db import models
+
+
+# Account manager
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+        if not kwargs.get('first_name'):
+            raise ValueError('Users must enter a valid first name.')
+
+        account = self.model(
+                email=self.normalize_email(email), **kwargs
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+    
+    def create_superuser(self, email, password):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_admin = True
+        account.save()
 
 # Account for a single user
 class Account(AbstractBaseUser):
+    # Validators
+    # Store phone number in E.164 std format
+    phone_regex = RegexValidator(regex=r'^\+?\d{9, 15}$', message="Phone number must be in +123456789 format, and between 9 and 15 digits.") 
+
+    # Data
     # Basic Info
-    first_name = models.CharField(max_length=20, blank=True)
+    first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=50, blank=True)
     about_me = models.CharField(max_length = 200, blank=True)
 
     # Contact info
-    email = Models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
     facebook = models.URLField(null=True, blank=True)
     twitter = models.URLField(null=True, blank=True)
-    phone_number = models.CharField(validators=[phone_regex], blank=True)
+    phone_number = models.CharField(max_length=16, validators=[phone_regex], blank=True)
 
-    # Validators
-
-    # Store phone number in E.164 std format
-    phone_regex = RegexValidator(regex=r'^\+?\d{9, 15}$', message="Phone number must be in +123456789 format, and between 9 and 15 digits.") 
 
     
 
